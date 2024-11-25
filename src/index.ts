4e1d0baf9d1986cb03b8086deefb36e68aeb2fa2
fioreenza <fiorenzanalle05@gmail.com>
2024-11-26 01:37:13 +0700

    fix: change localhost port

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.listen(PORT, () => {
 
 //izinkan CORS localhost:3000
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:5173');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
